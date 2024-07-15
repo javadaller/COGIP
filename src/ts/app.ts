36506fe57a8a6645b06aee7b34288cc7af49cb26
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-07-15 14:53:42 +0200

    ok

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,15 @@
-//test  /////
+import { connectToDatabase, closeDatabaseConnection } from './assets/js/server/connect/connect.js'
+
+const startApp = async () => {
+  try {
+    const connection = await connectToDatabase()
+
+    console.log('connected ',connection)
+    
+    await closeDatabaseConnection()
+  } catch (error) {
+    console.error('Error in startApp:', error)
+  }
+};
+
+startApp()
