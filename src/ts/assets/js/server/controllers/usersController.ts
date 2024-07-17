5011da70cc779eca74d48ec330ccb2fcc30e6bb4
NathAmd <nathanaelamd@gmail.com>
2024-07-17 11:11:37 +0200

    Register / secure

--- src/ts/assets/js/server/controllers/usersController.ts
+++ src/ts/assets/js/server/controllers/usersController.ts
@@ -19,41 +19,52 @@ export async function getAllUsers() {
 
 export async function loginUser(email: string, password: string) {
     try {
-        const connection = await connectToDatabase();
-        const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
-        const [rows] = await connection.query(sql, [email, password]) as any;
+        const connection = await connectToDatabase()
+        const sql = 'SELECT * FROM users WHERE email = ? AND password = ?'
+        const [rows] = await connection.query(sql, [email, password]) as any
 
-        console.log('User login:', rows);
+        console.log('User login:', rows)
 
-        await connection.end();
+        await connection.end()
 
-        return rows[0];
+        return rows[0]
     } catch (error) {
-        console.error('Error during user login:', error);
-        throw error;
+        console.error('Error during user login:', error)
+        throw error
     }
 }
 
 export async function registerUser(email: string, password: string, first_name: string) {
     try {
 
+
         if ((!email || !password || !first_name)) {
-            console.error("entry not valid");
-            return;
+            console.error("entry not valid")
+            return
         }
 
-        const connection = await connectToDatabase();
-        const sql = 'INSERT INTO users (email, password, first_name, role_ID, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)';
-        const now = new Date().toISOString().slice(0, 19).replace('T', ' ');
-        const [result] = await connection.query(sql, [email, password, first_name, 2, now, now]) as any;
+        const connection = await connectToDatabase()
 
-        console.log('User registered:', result);
+        const checkSql = 'SELECT * FROM users WHERE email = ? OR first_name = ?'
+        const [checkRows] = await connection.query(checkSql, [email, first_name]) as any
 
-        await connection.end();
+        if (checkRows.length > 0) {
+            console.error('Email or first name already exists')
+            return
+        }
 
-        return result;
+
+        const sql = 'INSERT INTO users (email, password, first_name, role_ID, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)'
+        const now = new Date().toISOString().slice(0, 19).replace('T', ' ')
+        const [result] = await connection.query(sql, [email, password, first_name, 2, now, now]) as any
+
+        console.log('User registered:', result)
+
+        await connection.end()
+
+        return result
     } catch (error) {
-        console.error('Error during user registration:', error);
-        throw error;
+        console.error('Error during user registration:', error)
+        throw error
     }
 }
