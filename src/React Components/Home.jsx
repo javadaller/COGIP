25750daf707605cb4f9c4de80e90e82b10018ea7
Xeless <tomdelintepro@gmail.com>
2024-07-18 21:21:47 +0200

    update home

--- src/React Components/Home.jsx
+++ src/React Components/Home.jsx
@@ -46,12 +46,83 @@ export default function Home() {
       </header>
 
       <body>
-        <section className="main-container">
-        <h2>Last invoices</h2>
-        </section>
-      </body>
+  <div class="main-container">
+    <section class="test">
+      <h2>Last invoices</h2>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Invoice number</th>
+              <th>Dates due</th>
+              <th>Company</th>
+              <th>Created at</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>F20220915-001</td>
+              <td>15/09/2022</td>
+              <td>Jouet Jean-Michel</td>
+              <td>25/09/2020</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+    <section class="test">
+      <h2>Last contacts</h2>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Contact name</th>
+              <th>Position</th>
+              <th>Company</th>
+              <th>Contacted at</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Jane Doe</td>
+              <td>Manager</td>
+              <td>Company XYZ</td>
+              <td>15/09/2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
 
-    
+    <section class="test">
+      <h2>Last compagnies</h2>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Contact name</th>
+              <th>Position</th>
+              <th>Company</th>
+              <th>Contacted at</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Jane Doe</td>
+              <td>Manager</td>
+              <td>Company XYZ</td>
+              <td>15/09/2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
+</body>
+
     </>
   );
 }
