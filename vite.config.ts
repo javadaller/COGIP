eb3d354663fd78da8ae84574c624215d47577a6d
Xeless <tomdelintepro@gmail.com>
2024-07-18 11:06:41 +0200

    update error page

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: [
-      'react-icons',
-      'prop-types',
-    ],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    open: true,
   },
-})
+});
